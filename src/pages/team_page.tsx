b23718409c67bcdf305000d80193aa8dc3992902
clairerg <claireruyi.guo@gmail.com>
2024-08-19 22:01:21 -0400

    add photos

--- src/pages/team_page.tsx
+++ src/pages/team_page.tsx
@@ -8,8 +8,7 @@ interface TeamElementProps {
 
 function OfficerElement(props: TeamElementProps) {
     return (
-        <div className="flex flex-row items-center justify-end    p-4">
-            
+        <div className="flex flex-row items-center justify-end p-4">            
             <div className="mr-4 flex flex-1 flex-col items-center">
             <img src={props.image_path} className="size-56 rounded-full" />
                 <div className="text-nowrap text-2xl">{props.name}</div>
@@ -23,7 +22,7 @@ function OfficerElement(props: TeamElementProps) {
 
 function TeamPage() {
     return (
-        <div className="mx-8 mt-16 text-4xl content-center w-11/12">
+        <div className="mx-8 mt-10 text-4xl content-center w-11/12">
             <div className="w-full text-center text-6xl">Leadership</div>
             
             <div className="mt-8 grid grid-cols-3 gap-6">
