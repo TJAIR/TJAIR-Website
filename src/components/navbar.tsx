ae9cf2ba2321da888f746d60a7d8d2b5cb464a57
Justin Lee <justin.adam.lee@gmail.com>
2024-10-09 17:48:30 -0400

    update logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "/logo.png";
+import Logo from "/logo.svg";
 import { routes } from "../shared";
 
 function NavbarElement({ name, path }: { name: string; path: string }) {
